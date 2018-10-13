b9a1e4b031c72b6a1928f4684cd434c489424602
Mitchell <mitchellberry@protonmail.com>
2018-10-13 10:42:38 +0800

    moved tools class to tools.cs

--- Makwa/tools.cs
+++ Makwa/tools.cs
@@ -1,219 +1,155 @@
 ﻿using System;
+using Makwa.BigInt;
 
-namespace Mono.Security
+namespace Makwa
 {
-    internal sealed class BitConverterLE
+    public class Tools
     {
-        private BitConverterLE()
+        /// <summary>
+        ///  Integer to Octet Stream Primitive, represents a Big Integer as a byte array
+        /// </summary>
+        public static byte[] I2OSP(BigInteger x)
         {
+            return I2OSP(x, x);
         }
 
-        unsafe private static byte[] GetUShortBytes(byte* bytes)
+        /// <summary>
+        ///  Integer to Octet Stream Primitive, represents a Big Integer as a 
+        ///  byte array of length modulus
+        /// </summary>
+        public static byte[] I2OSP(BigInteger x, BigInteger modulus)
         {
-            if (BitConverter.IsLittleEndian)
-                return new byte[] { bytes[0], bytes[1] };
-            else
-                return new byte[] { bytes[1], bytes[0] };
-        }
-
-        unsafe private static byte[] GetUIntBytes(byte* bytes)
-        {
-            if (BitConverter.IsLittleEndian)
-                return new byte[] { bytes[0], bytes[1], bytes[2], bytes[3] };
-            else
-                return new byte[] { bytes[3], bytes[2], bytes[1], bytes[0] };
-        }
-
-        unsafe private static byte[] GetULongBytes(byte* bytes)
-        {
-            if (BitConverter.IsLittleEndian)
-                return new byte[] { bytes [0], bytes [1], bytes [2], bytes [3],
-                             bytes [4], bytes [5], bytes [6], bytes [7] };
-            else
-                return new byte[] { bytes [7], bytes [6], bytes [5], bytes [4],
-                             bytes [3], bytes [2], bytes [1], bytes [0] };
-        }
-
-        unsafe internal static byte[] GetBytes(bool value)
-        {
-            return new byte[] { value ? (byte)1 : (byte)0 };
-        }
-
-        unsafe internal static byte[] GetBytes(char value)
-        {
-            return GetUShortBytes((byte*)&value);
-        }
-
-        unsafe internal static byte[] GetBytes(short value)
-        {
-            return GetUShortBytes((byte*)&value);
-        }
-
-        unsafe internal static byte[] GetBytes(int value)
-        {
-            return GetUIntBytes((byte*)&value);
-        }
-
-        unsafe internal static byte[] GetBytes(long value)
-        {
-            return GetULongBytes((byte*)&value);
-        }
-
-        unsafe internal static byte[] GetBytes(ushort value)
-        {
-            return GetUShortBytes((byte*)&value);
-        }
-
-        unsafe internal static byte[] GetBytes(uint value)
-        {
-            return GetUIntBytes((byte*)&value);
-        }
-
-        unsafe internal static byte[] GetBytes(ulong value)
-        {
-            return GetULongBytes((byte*)&value);
-        }
-
-        unsafe internal static byte[] GetBytes(float value)
-        {
-            return GetUIntBytes((byte*)&value);
-        }
-
-        unsafe internal static byte[] GetBytes(double value)
-        {
-            return GetULongBytes((byte*)&value);
-        }
-
-        unsafe private static void UShortFromBytes(byte* dst, byte[] src, int startIndex)
-        {
-            if (BitConverter.IsLittleEndian)
+            int len = (modulus.BitLength + 7) >> 3;
+            byte[] b = x.ToByteArray();
+            int blen = b.Length;
+            if (blen < len)
             {
-                dst[0] = src[startIndex];
-                dst[1] = src[startIndex + 1];
+                byte[] nb = new byte[len];
+                Array.Copy(b, 0, nb, len - blen, blen);
+                return nb;
+            }
+            else if (blen == len)
+            {
+                return b;
             }
             else
             {
-                dst[0] = src[startIndex + 1];
-                dst[1] = src[startIndex];
+                byte[] nb = new byte[len];
+                Array.Copy(b, blen - len, nb, 0, len);
+                return nb;
             }
+
         }
 
-        unsafe private static void UIntFromBytes(byte* dst, byte[] src, int startIndex)
+        /// <summary>
+        /// Octet Stream to Integer Primitive, converts a byte array to BigInt
+        /// </summary>
+        public static BigInteger OS2IP(byte[] b, BigInteger modulus)
         {
-            if (BitConverter.IsLittleEndian)
+            int len = (modulus.BitLength + 7) >> 3;
+            int blen = b.Length;
+            if (blen != len)
             {
-                dst[0] = src[startIndex];
-                dst[1] = src[startIndex + 1];
-                dst[2] = src[startIndex + 2];
-                dst[3] = src[startIndex + 3];
+                throw new ArgumentOutOfRangeException("Invalid integer input");
             }
-            else
+            if (b[0] < 0)
             {
-                dst[0] = src[startIndex + 3];
-                dst[1] = src[startIndex + 2];
-                dst[2] = src[startIndex + 1];
-                dst[3] = src[startIndex];
+                byte[] nb = new byte[blen + 1];
+                Array.Copy(b, 0, nb, 1, blen);
+                b = nb;
             }
-        }
-
-        unsafe private static void ULongFromBytes(byte* dst, byte[] src, int startIndex)
-        {
-            if (BitConverter.IsLittleEndian)
+            BigInteger x = new BigInteger(1, b);
+            if (x.CompareTo(modulus) >= 0)
             {
-                for (int i = 0; i < 8; ++i)
-                    dst[i] = src[startIndex + i];
+                throw new ArgumentOutOfRangeException("Invalid integer input");
             }
-            else
+            return x;
+        }
+
+        /// <summary>
+        /// Decodes a byte array into an unpadded Base64 string
+        /// </summary>
+        public static string DecodeBase64(byte[] m)
+        {
+            return Convert.ToBase64String(m).Replace("=", "");
+        }
+
+        /// <summary>
+        /// Encodes an unpadded Base64 string
+        /// </summary>
+        public static byte[] EncodeBase64(string m)
+        {
+            //int len = ((4 - (m.Length % 4) % 4));
+            int len = (m.Length % 4);
+            string padding = new string('=', len);
+            return Convert.FromBase64String(m + padding);
+        }
+
+        /// <summary>
+        /// Transforms hex strings into a byte array 
+        /// </summary>
+        public static byte[] HexStringToByteArray(String hexstring)
+        {
+            int NumberChars = hexstring.Length;
+            byte[] bytes = new byte[NumberChars / 2];
+            for (int i = 0; i < NumberChars; i += 2)
+                bytes[i / 2] = Convert.ToByte(hexstring.Substring(i, 2), 16);
+            return bytes;
+        }
+
+        /// <summary>
+        /// Performs a byte by byte xor comparison of two arrays for the purpose 
+        /// of password verification, checks every byte regardless of failure,
+        /// used to prevent timing attacks.
+        public static bool ConstantTimeComparison(byte[] a, byte[] b)
+        {
+            uint diff = (uint)a.Length ^ (uint)b.Length;
+            for (uint i = 0; i < a.Length && i < b.Length; i++)
             {
-                for (int i = 0; i < 8; ++i)
-                    dst[i] = src[startIndex + (7 - i)];
+                diff |= (uint)(a[i] ^ b[i]);
             }
+            return diff == 0;
         }
 
-        unsafe internal static bool ToBoolean(byte[] value, int startIndex)
+        /// <summary>
+        /// This implementation enforces specific work factors of the form w = ζ · 2ᵟ
+        /// , where ζ = 2 or 3, and δ ≥ 0    
+        /// </summary>
+        public static bool InvalidWorkfactor(uint workfactor)
         {
-            return value[startIndex] != 0;
+            if (workfactor == 0) { return true; }
+            bool checkthree = IsPowerofTwo(workfactor / 3);
+            bool checktwo = IsPowerofTwo(workfactor / 2);
+            return (!(checkthree || checktwo));
         }
 
-        unsafe internal static char ToChar(byte[] value, int startIndex)
+        static bool IsPowerofTwo(uint x)
         {
-            char ret;
-
-            UShortFromBytes((byte*)&ret, value, startIndex);
-
-            return ret;
+            return (x != 0) && ((x & (x - 1)) == 0);
         }
 
-        unsafe internal static short ToInt16(byte[] value, int startIndex)
+        /// <summary>
+        /// Provides the closest workfactor, when an invalid workfactor is supplied
+        /// </summary>
+        public static uint SuggestWorkFactor(uint workFactor)
         {
-            short ret;
-
-            UShortFromBytes((byte*)&ret, value, startIndex);
-
-            return ret;
-        }
-
-        unsafe internal static int ToInt32(byte[] value, int startIndex)
-        {
-            int ret;
-
-            UIntFromBytes((byte*)&ret, value, startIndex);
-
-            return ret;
-        }
-
-        unsafe internal static long ToInt64(byte[] value, int startIndex)
-        {
-            long ret;
-
-            ULongFromBytes((byte*)&ret, value, startIndex);
-
-            return ret;
-        }
-
-        unsafe internal static ushort ToUInt16(byte[] value, int startIndex)
-        {
-            ushort ret;
-
-            UShortFromBytes((byte*)&ret, value, startIndex);
-
-            return ret;
-        }
-
-        unsafe internal static uint ToUInt32(byte[] value, int startIndex)
-        {
-            uint ret;
-
-            UIntFromBytes((byte*)&ret, value, startIndex);
-
-            return ret;
-        }
-
-        unsafe internal static ulong ToUInt64(byte[] value, int startIndex)
-        {
-            ulong ret;
-
-            ULongFromBytes((byte*)&ret, value, startIndex);
-
-            return ret;
-        }
-
-        unsafe internal static float ToSingle(byte[] value, int startIndex)
-        {
-            float ret;
-
-            UIntFromBytes((byte*)&ret, value, startIndex);
-
-            return ret;
-        }
-
-        unsafe internal static double ToDouble(byte[] value, int startIndex)
-        {
-            double ret;
-
-            ULongFromBytes((byte*)&ret, value, startIndex);
-
-            return ret;
+            uint[] validWorkFactors = { 6, 8, 12, 16, 24, 32, 48, 64, 96, 128, 192, 256, 384, 512,
+                768, 1024, 1536, 2048, 3072, 4096, 6144, 8192, 12288, 16384, 24576, 32768, 49152,
+                65536, 98304, 131072, 196608, 262144, 393216, 524288, 786432, 1048576, 1572864 };
+            uint[] distances = new uint[validWorkFactors.Length];
+            uint smallestDistance = 1572864;
+            uint closestValidWorkFactor = 1572864;
+            uint distance = new int();
+            for (int i = 0; i < validWorkFactors.Length; i++)
+            {
+                distance = (uint)Math.Abs(validWorkFactors[i] - workFactor);
+                if (distance < smallestDistance)
+                {
+                    smallestDistance = distance;
+                    closestValidWorkFactor = validWorkFactors[i];
+                }
+            }
+            return closestValidWorkFactor;
         }
     }
 }
